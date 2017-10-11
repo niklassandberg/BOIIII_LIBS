16cccbec429810207a1286a5072b307696079684
Niklas Sandberg <niklas1sandberg@gmail.com>
2017-10-11 13:50:54 +0200

    optimize: toneSlide filter or prevouse named glidefilters #3

--- bomath.h
+++ bomath.h
@@ -5,23 +5,39 @@ inline uint32_t mulu8x16_24(uint8_t uint8in, uint16_t uint16in)
 {
   uint32_t res;
   asm volatile (
-  "clr r16 \n\t"
+  "clr %D0 \n\t"
+  "clr %C0 \n\t"
   "mul %A1, %A2 \n\t"
-  "movw %A0, r0 \n\t"
-  "mov %C0, r16 \n\t"
-  "mov %D0, r16 \n\t"
+  "movw %A0, __tmp_reg__ \n\t"
   "mul %A1, %B2 \n\t"
-  "add %B0, r0 \n\t"
-  "adc %C0, r1 \n\t"
-  "adc %D0, r16 \n\t"
+  "add %B0, __tmp_reg__ \n\t"
+  "adc %C0, __zero_reg__ \n\t"
   "clr r1 \n\t"
   :
   "=&r" (res)
   :
   "a" (uint8in),
   "a" (uint16in)
+  );
+  return res;
+}
+
+inline uint16_t mulu8x16div8_16(uint8_t uint8in, uint16_t uint16in)
+{
+  uint16_t res;
+  asm volatile (
+  "clr %B0 \n\t"
+  "mul %A1, %A2 \n\t"
+  "mov %A0, __zero_reg__ \n\t"
+  "mul %A1, %B2 \n\t"
+  "add %A0, __tmp_reg__ \n\t"
+  "adc %B0, __zero_reg__ \n\t"
+  "clr r1 \n\t"
   :
-  "r16"
+  "=&r" (res)
+  :
+  "a" (uint8in),
+  "a" (uint16in)
   );
   return res;
 }
