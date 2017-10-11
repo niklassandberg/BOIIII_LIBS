873117fb1b44ed746ecd83b42aa3d5dddca351fa
Niklas Sandberg <niklas1sandberg@gmail.com>
2017-10-11 14:56:06 +0200

    optimize: toneSlide filter or prevouse named glidefilters #3

--- bofilters.cpp
+++ bofilters.cpp
@@ -4,17 +4,17 @@ void toneSlide( int16_t in, int16_t& out, int8_t midiCC )
 {
   if(midiCC < 1) { out = in; return; }
   else if(in==out) { return; }
-  midiCC = 127 - midiCC;
+  midiCC = (127 - midiCC) << 1; // 0-127 => 0-255
   int16_t o = (in - out);
   int16_t c;
   if(o > 0)
   {
-    c = mulu8x16_24( midiCC , o ) >> 7;
+    c = mulu8x16div8_16( midiCC , o );
     if(!c) c = 1;
   }
   else
   {
-    c = - (mulu8x16_24( midiCC , -o ) >> 7);
+    c = - mulu8x16div8_16( midiCC , -o );
     if(!c) c = -1;
   }
   out += c;
@@ -26,9 +26,8 @@ void toneSlide2(int16_t in, int16_t & out, int8_t midiCC)
   static int16_t accumilator = 0;
   if(midiCC < 1) { out = in; return; }
   else if(in==out) { return; }
+  if(midiCC > 119) midiCC = 119; //because if becomes unstable otherwice.
   midiCC = 127 - midiCC;
-  if(midiCC < 16) midiCC = 16; //because if becomes unstable otherwice.
-  //TODO: AVR201.asm
-  out = mulu8x16div8_16( midiCC, accumilator << 1);
+  out = mulu8x16div8_16( midiCC << 1, accumilator);
   accumilator = accumilator - out + in;
 }
